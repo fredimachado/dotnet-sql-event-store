e5ae5fd5aacae98dcc591e80336343d6a1a445db
Fredi Machado <fredisoft@gmail.com>
2023-08-13 11:16:05 +1000

    Separate SqliteFixture class

--- tests/Warehouse.Tests/EventStore/SqliteEventStoreTests.cs
+++ tests/Warehouse.Tests/EventStore/SqliteEventStoreTests.cs
@@ -291,26 +291,3 @@ public class SqliteEventStoreTests : IClassFixture<SqliteFixture>, IAsyncDisposa
         return _sut.DisposeAsync();
     }
 }
-
-public class SqliteFixture : IDisposable
-{
-    public const string ConnectionString = "Data Source=InMemorySample;Mode=Memory;Cache=Shared";
-
-    private readonly SqliteConnection _sqliteConnection;
-    public SqliteFixture()
-    {
-        var path = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "sqlite-event-store.ddl");
-        var commands = File.ReadAllText(path);
-
-        _sqliteConnection = new SqliteConnection(ConnectionString);
-        _sqliteConnection.Open();
-
-        var command = new SqliteCommand(commands, _sqliteConnection);
-        command.ExecuteNonQuery();
-    }
-
-    public void Dispose()
-    {
-        _sqliteConnection.Dispose();
-    }
-}
