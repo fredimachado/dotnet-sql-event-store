7a0875d154282e0f74c7bbc51e58ca8ea5ec1503
Fredi Machado <fredisoft@gmail.com>
2023-08-13 12:37:15 +1000

    Test Sqlite event store

---
+++ tests/Warehouse.Tests/Infrastructure/SqliteEventStoreTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.Sqlite;
+using WareHouseApi.Domain.Entities;
+using WareHouseApi.Domain.Events;
+using WareHouseApi.Infrastructure;
+
+namespace Warehouse.Tests.Infrastructure;
+
+public class SqliteEventStoreTests : IClassFixture<SqliteFixture>, IAsyncDisposable
+{
+    private readonly SqliteEventStore _eventStore;
+    private readonly SqliteFixture _fixture;
+
+    public SqliteEventStoreTests(SqliteFixture fixture)
+    {
+        _fixture = fixture;
+        _eventStore = new SqliteEventStore(new SqliteConnection(SqliteFixture.ConnectionString));
+    }
+
+    [Fact]
+    public async Task Test_Saving_Event()
+    {
+        var id = Guid.NewGuid();
+        var warehouseProduct = new WarehouseProduct(id);
+        warehouseProduct.ReceiveProduct(10);
+
+        await _eventStore.SaveEventsAsync(warehouseProduct, warehouseProduct.GetPendingEvents());
+
+        var events = await _eventStore.GetEventsAsync(warehouseProduct);
+
+        var @event = events.ShouldHaveSingleItem();
+        var productReceived = @event.ShouldBeOfType<ProductReceived>();
+        productReceived.Quantity.ShouldBe(10);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _eventStore.DisposeAsync();
+        await _fixture.DisposeAsync();
+    }
+}
---
+++ tests/Warehouse.Tests/Infrastructure/SqliteFixture.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.Sqlite;
+using WareHouseApi.Domain;
+using WareHouseApi.Infrastructure;
+
+namespace Warehouse.Tests.Infrastructure;
+
+public class SqliteFixture : IAsyncDisposable
+{
+    public const string ConnectionString = "Data Source=EventStore;Mode=Memory;Cache=Shared";
+
+    private readonly SqliteConnection _sqliteConnection;
+    public SqliteFixture()
+    {
+        EventTypeResolver.RegisterEvents(typeof(Event).Assembly);
+
+        _sqliteConnection = new SqliteConnection(ConnectionString);
+        _sqliteConnection.Open();
+
+        RunInitializationScript();
+
+        AddEntityEvents();
+    }
+
+    private void AddEntityEvents()
+    {
+        var entityName = "WarehouseProduct";
+        var commands = $@"
+            INSERT INTO entity_events (entity, event) VALUES ('{entityName}', 'ProductReceived');
+            INSERT INTO entity_events (entity, event) VALUES ('{entityName}', 'ProductShipped');
+            INSERT INTO entity_events (entity, event) VALUES ('{entityName}', 'InventoryAdjusted');
+            ";
+        var command = new SqliteCommand(commands, _sqliteConnection);
+        command.ExecuteNonQuery();
+    }
+
+    private void RunInitializationScript()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "sqlite-event-store.ddl");
+        var commands = File.ReadAllText(path);
+
+        var command = new SqliteCommand(commands, _sqliteConnection);
+        command.ExecuteNonQuery();
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        return _sqliteConnection.DisposeAsync();
+    }
+}
